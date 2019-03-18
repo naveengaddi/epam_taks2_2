53b58e1e2792398126958af694e46c2470c5d07d
naveengaddi <46519632+naveengaddi@users.noreply.github.com>
2019-03-18 21:37:56 +0530

    Add files via upload

---
+++ Calculate.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author naveen
+ */
+@WebServlet(urlPatterns = {"/Calculate"})
+public class Calculate extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        // Set response content type
+      response.setContentType("text/html");
+
+      PrintWriter out = response.getWriter();
+      //String title = "Epam-task";
+         double pr, rate, t, sim,com;
+       // Scanner sc=new Scanner (System. in);
+       // System.out.println("Enter the amount:");
+        pr=Float.parseFloat(request.getParameter("pr"));
+       // System. out. println("Enter the No.of years:");
+        t=Float.parseFloat(request.getParameter("t"));
+        //System. out. println("Enter the Rate of  interest");
+        rate=Float.parseFloat(request.getParameter("rate"));
+        sim=(pr * t * rate)/100;
+        com=pr * Math.pow(1.0+rate/100.0,t) - pr;
+        out.println("Simple Interest = "+sim);
+        out.println("Compound Interest = "+com);
+      
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
